ad9d1c58d7e66c9d215498ce0d488376878b4f4c
Malik Ismail <malik@emq.com>
2026-04-03 16:22:29 +0800

    update home message to v2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask CI/CD!"
+    return "Hello from Flask CI/CD! Version 2 - deployed by Jenkins!"
 
 
 @app.route("/health")
